ea5f3709ac060f2a2a4c34618d732e5015c44c4e
valeriagdlo99 <58562805+valeriagdlo@users.noreply.github.com>
2021-04-14 20:35:12 -0500

    initial commit for home

---
+++ Ninosyninas/resources/views/home.blade.php
@@ -0,0 +1,99 @@
+@extends('layouts.main-landing')
+
+@section('pageTitle', "Niños y niñas de Mexico")
+
+@section('header')
+<div class='upper-half'> 
+<br>
+<br>
+<h1 class='text-center'>¡Bienvenidos!</h1>
+<br>
+<br>
+@endsection
+
+@section('mainContent')
+<div class="container">
+<div class="row">
+  <div class="col-4" >
+    <div class="card w-100 h-75">
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.fdsfsdfdsfsdfdfdsfsdfdfsdfsdfdf</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-4">
+    <div class="card w-100 h-75">
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-4">
+    <div class="card w-100 h-75">
+      <div class="card-body ">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+<div class="card  w-25">
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+
+
+</div>
+<div class="card  w-25">
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+
+
+</div>
+<div class="card  w-25">
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+
+
+</div>
+
+
+<div class="row">
+  <div class="col-sm-6">
+    
+  </div>
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+@endsection
