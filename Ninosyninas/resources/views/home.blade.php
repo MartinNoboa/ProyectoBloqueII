c5d79a38fff2538f6a258d9a634aa0e85f69bdcc
valeriagdlo99 <58562805+valeriagdlo@users.noreply.github.com>
2021-04-14 22:10:06 -0500

    first initial front home

--- Ninosyninas/resources/views/home.blade.php
+++ Ninosyninas/resources/views/home.blade.php
@@ -6,94 +6,135 @@
 <div class='upper-half'> 
 <br>
 <br>
-<h1 class='text-center'>¡Bienvenidos!</h1>
+<h1 class='text-center'>¡Bienvenido!</h1>
 <br>
 <br>
 @endsection
 
 @section('mainContent')
 <div class="container">
-<div class="row">
-  <div class="col-4" >
-    <div class="card w-100 h-75">
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.fdsfsdfdsfsdfdfdsfsdfdfsdfsdfdf</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
+    <div class="row">
+    <div class="col-12 col-md-4" >
+        <div class="card w-100 h-100 shadow">
+        <div class="card-body text-center ">
+            <img src="{{url('img/fondo1.jpg')}}" class="rounded-circle shadow" alt="Cinque Terre" width="150" height="150"> 
+            <br>
+            <br>
+            <h3>Usarios</h3>
+            <h5> loren ipsum loren ipsum loren ipsum loren ipsum</h5>
+            <button type="button" class="btn btn-light">Abrir</button>
+        </div>
+        </div>
     </div>
-  </div>
-  <div class="col-4">
-    <div class="card w-100 h-75">
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
+    <br>        
+    <div class="col-12 col-md-4 ">
+        <div class="card w-100 h-100 shadow">
+        <div class="card-body text-center">
+            <img src="{{url('img/fondo1.jpg')}}" class="rounded-circle shadow" alt="Cinque Terre" width="150" height="150"> 
+            <br>
+            <br>
+            <h3>Niños</h3>
+            <h5> loren ipsum loren ipsum loren ipsum loren ipsum</h5>
+            <button type="button" class="btn btn-light">Abrir</button>
+        </div>
+        </div>
     </div>
-  </div>
-  <div class="col-4">
-    <div class="card w-100 h-75">
-      <div class="card-body ">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
+    <br>
+    <div class="col-12 col-md-4">
+        <div class="card w-100 h-100 shadow">
+        <div class="card-body text-center">
+            <img src="{{url('img/fondo1.jpg')}}" class="rounded-circle shadow" alt="Cinque Terre" width="150" height="150"> 
+            <br>
+            <br>
+            <h3>Calendario</h3>
+            <h5> loren ipsum loren ipsum loren ipsum loren ipsum</h5>
+            <button type="button" class="btn btn-light">Abrir</button>
+        </div>
+        </div>
     </div>
-  </div>
-</div>
-
-
-
-
-
-
-<div class="card  w-25">
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
     </div>
-
-
-</div>
-<div class="card  w-25">
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
-
-
-</div>
-<div class="card  w-25">
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
-
-
-</div>
-
-
-<div class="row">
-  <div class="col-sm-6">
+    <br>
+    <br>
     
-  </div>
-  <div class="col-sm-6">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
+
+
+
+    <div class="row">
+    <div class="col-12 col-md-4" >
+        <div class="card w-100 h-100 shadow">
+        <div class="card-body text-center ">
+            <img src="{{url('img/fondo1.jpg')}}" class="rounded-circle shadow" alt="Cinque Terre" width="150" height="150"> 
+            <br>
+            <br>
+            <h3>Noticias</h3>
+            <h5> loren ipsum loren ipsum loren ipsum loren ipsum</h5>
+            <button type="button" class="btn btn-light">Abrir</button>
+        </div>
+        </div>
     </div>
-  </div>
+    <br>       
+    <div class="col-12 col-md-4 ">
+        <div class="card w-100 h-100 shadow">
+        <div class="card-body text-center">
+            <img src="{{url('img/fondo1.jpg')}}" class="rounded-circle shadow" alt="Cinque Terre" width="150" height="150"> 
+            <h3>Progreso</h3>
+            <h5> loren ipsum loren ipsum loren ipsum loren ipsum</h5>
+            <button type="button" class="btn btn-light">Abrir</button>
+        </div>
+        </div>
+    </div>
+    <br>
+    <div class="col-12 col-md-4">
+        <div class="card w-100 h-100 shadow">
+        <div class="card-body text-center">
+        <img src="{{url('img/fondo1.jpg')}}" class="rounded-circle shadow" alt="Cinque Terre" width="150" height="150"> 
+            <h3>Donaciones</h3>
+            <h5> loren ipsum loren ipsum loren ipsum loren ipsum</h5>
+            <button type="button" class="btn btn-light">Abrir</button>
+        </div>
+        </div>
+    </div>
+    </div>
+    <br>
+    <br>
+
+    <div class="row">
+    <div class="col-12 col-md-4" >
+        <div class="card w-100 h-100 shadow">
+        <div class="card-body text-center ">
+            <img src="{{url('img/fondo1.jpg')}}" class="rounded-circle shadow" alt="Cinque Terre" width="150" height="150"> 
+            <br>
+            <br>
+            <h3>Galeria</h3>
+            <h5> loren ipsum loren ipsum loren ipsum loren ipsum</h5>
+            <button type="button" class="btn btn-light">Abrir</button>
+        </div>
+        </div>
+    </div>
+    <br>       
+    <div class="col-12 col-md-4 ">
+        <div class="card w-100 h-100 shadow">
+        <div class="card-body text-center">
+            <img src="{{url('img/fondo1.jpg')}}" class="rounded-circle shadow" alt="Cinque Terre" width="150" height="150"> 
+            <h3>Reportes</h3>
+            <h5> loren ipsum loren ipsum loren ipsum loren ipsum</h5>
+            <button type="button" class="btn btn-light">Abrir</button>
+        </div>
+        </div>
+    </div>
+    <br>
+    </div>
+    <br>
+    <br>
+
+
+
 </div>
+
+
+
+
+
 
 
 @endsection
