90fd5545a497eeb24c309e00b4410ee84b03704b
FVargas4 <fvargascode@gmail.com>
2021-05-24 18:57:14 -0500

    Correccion de seeder de ninos

--- Ninosyninas/database/seeders/DatabaseSeeder.php
+++ Ninosyninas/database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
             RoleSeeder::class,
             PermitSeeder::class,
             areasSeeder::class,
+            ChilderSeeder::class,
            
             //Tablas de relacion
             PermitRoleSeeder::class,
