9a032e3b55f0c212e702286b543a27aaad944311
FVargas4 <fvargascode@gmail.com>
2021-05-24 08:36:44 -0500

    Caso en que no haya noticias disponibles

--- Ninosyninas/resources/views/landing/landing-noticias.blade.php
+++ Ninosyninas/resources/views/landing/landing-noticias.blade.php
@@ -13,21 +13,27 @@
 @endsection
 
 @section('mainContent')
-<div class="container center">
-    <div class="row">
-        @foreach($noticias as $noticia)
-        <div class="col-sm-6">
-            <div class="card noticia m-2" style="width: 25rem;">
-                <img src="{{asset($noticia->image->url)}}" class="card-img-top" alt="Imagen de la noticia">
-                <div class="card-body text-center">
-                    <h5 class="card-title">{{$noticia -> titulo}}</h5>
-                    <p class="card-text">{{$noticia -> cuerpo}}</p>
-                    <a href="{{url('/noticia/ver/'.$noticia->id) }}" class="btn btn-primary" alt="enlace a noticia">Ver Noticia Completa</a>
+@if($noticias -> count())
+    <div class="container center">
+        <div class="row">
+            @foreach($noticias as $noticia)
+            <div class="col-sm-6">
+                <div class="card noticia m-2" style="width: 25rem;">
+                    <img src="{{asset($noticia->image->url)}}" class="card-img-top" alt="Imagen de la noticia">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">{{$noticia -> titulo}}</h5>
+                        <p class="card-text">{{$noticia -> cuerpo}}</p>
+                        <a href="{{url('/noticia/ver/'.$noticia->id) }}" class="btn btn-primary" alt="enlace a noticia">Ver Noticia Completa</a>
+                    </div>
                 </div>
             </div>
-        </div>
-        @endforeach
+            @endforeach
        
+        </div>
     </div>
-</div>
+@else
+    <div>
+        <h2 class="text-center font-title">No hay noticias disponibles</h2>
+    </div>
+@endif
 @endsection
