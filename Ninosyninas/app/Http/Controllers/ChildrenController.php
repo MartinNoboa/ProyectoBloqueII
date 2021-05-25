d23c361d67a1c6c48a9fd0fe0a783903ee163dd2
Omar Molina <A01423485@itesm.mx>
2021-05-24 20:33:44 -0500

    usuarios

--- Ninosyninas/app/Http/Controllers/ChildrenController.php
+++ Ninosyninas/app/Http/Controllers/ChildrenController.php
@@ -20,7 +20,7 @@ class ChildrenController extends Controller
 
         $children=DB::table('children')
                     ->select('children.id','children.nombre','children.apellido_paterno','children.apellido_materno', 'children.fecha_nacimiento', 'children.grado', DB::raw('round(AVG(reportes.calificacion),0) as promedio'))
-                    ->join('reportes','reportes.child_id',"=",'children.id')
+                    ->leftJoin('reportes','reportes.child_id',"=",'children.id')
                     ->groupBy('id','nombre' , 'apellido_paterno','apellido_materno', 'fecha_nacimiento', 'grado')  
 
                     ->get();
