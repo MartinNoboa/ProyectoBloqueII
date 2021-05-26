539472caa07916c0589eec1913a775d5b9f64b94
Sebastian R <sebas.r.2599@gmail.com>
2021-05-25 21:54:35 -0500

    Arreglado defectos

--- Ninosyninas/resources/views/galeria/galeria-home.blade.php
+++ Ninosyninas/resources/views/galeria/galeria-home.blade.php
@@ -1,14 +1,24 @@
 @extends('layouts.main-landing')
 @section('content')
-<div class='background cont pb-5'> 
+<div class='background cont pb-5'>
     <h1 class='text-center titulo-rojo font-title-title pt-5 pb-5'>Galería</h1>
-    <div class="grid-gallery m-0 p-4">
-        
-        @foreach ($galleries as $galleriesItem)
-        <div class="gallery-item m-0 shadow-md">
-            <img class="gallery-image" src="{{asset($galleriesItem->image->url)}} " alt="no se encontro la imagen" srcset="">
+    @if ($galleries->count())
+        <div class="grid-gallery m-0 p-4">
+
+            @foreach ($galleries as $galleriesItem)
+                <div class="gallery-item m-0 shadow-md">
+                    <img class="gallery-image" src="{{asset($galleriesItem->image->url)}} " alt="no se encontro la imagen"
+                        srcset="">
+                </div>
+            @endforeach
         </div>
-        @endforeach
-    </div>
+    @else
+        <div class="empty-space">
+            <h2 class="my-3 text-center pt-5">
+                No hay fotos en la disponibles en la galeria
+            </h2>
+        </div>
+    @endif
+
 </div>
 @endsection
